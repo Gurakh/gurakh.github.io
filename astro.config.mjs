7c373f028bce3387f1d657c4880f38ae9243eedc
Franck Da Silva <franck@antistatique.net>
2024-11-22 11:56:00 +0100

    Add correct domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,5 @@ import tailwind from '@astrojs/tailwind';
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  site: 'https://gurakh.github.io'
+  site: 'https://franckds.ch'
 });
